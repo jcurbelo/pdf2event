96bd1c9131515ee542aa18e6f2a9c6efa993bb7e
jcurbelo <curbelorobin@gmail.com>
2021-08-05 22:34:19 -0400

    Add pdf_parser util functions

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from io import BytesIO, FileIO
 from typing import List
 from fastapi import FastAPI, Request, UploadFile, File
 from fastapi.templating import Jinja2Templates
+from utils.pdf_parser import parse_files
 
 templates = Jinja2Templates(directory="templates")
 
@@ -9,9 +11,14 @@ app = FastAPI()
 
 @app.post("/upload-files")
 async def upload_files(files: List[UploadFile] = File(...)):
-    return {"filenames": [file.filename for file in files]}
+    return {"result": parse_files([get_file_io(f) for f in files])}
 
 
 @app.get("/")
 async def root(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
+
+
+# returns file-like object
+def get_file_io(file: UploadFile):
+    return BytesIO(file.file.read())
---
+++ utils/pdf_parser.py
@@ -0,0 +1,87 @@
+from typing import List, IO
+from pdfminer.high_level import extract_text
+import re
+import datefinder
+
+EVENT_TITLES = [
+    'ADDITION OF ANY NEW PARTIES',
+    'FACT WITNESS LIST',
+    'EXHIBIT LIST',
+    'EXPERT WITNESSES',
+    'FACT AND EXPERT DISCOVERY',
+    'MOTIONS FOR SUMMARY JUDGMENT and DAUBERT MOTIONS',
+    'OBJECTIONS TO PLEADINGS AND ALL OTHER PRETRIAL',
+    'MEDIATION',
+    'TRIAL',
+    'JOINT PRETRIAL STIPULATION',
+    'DONE and ORDERED'
+]
+
+# number of dates per title
+DATES_COUNT = [
+    1,
+    1,
+    1,
+    1,
+    2,
+    2,
+    2,
+    1,
+    1,
+    1,
+]
+
+def extract_case_number(text: str) -> str:
+    m = re.search(r"Case No: ([^\n]+)", text, re.MULTILINE | re.IGNORECASE)
+    if m:
+        return m.group(1)
+    return ''
+
+def clean_text(text: str) -> str:
+    # remove unwanted content
+    text = re.sub(r"Page \d+ of \d+", '', text)
+    text = re.sub(r"Case No: [^\n]+", '', text)
+    # remove extra empty lines
+    lines = text.split('\n')
+    lines = [line for line in lines if line.strip()]
+    text = '\n'.join(lines)
+    return text
+
+
+def extract_chunks(text: str) -> dict:
+    chunks = {}
+    lines = text.split('\n')
+    current_idx = 0
+    current_chunk = ''
+    current_title = None
+    next_title = EVENT_TITLES[current_idx]
+    for l in lines:
+        if next_title in l:
+            if current_title is not None:
+                chunks[current_title] = current_chunk
+            current_chunk = ''
+            # set up for next title
+            if current_idx == len(EVENT_TITLES) - 1:
+                break
+            current_idx += 1
+            current_title = next_title
+            next_title = EVENT_TITLES[current_idx]
+        if current_title is not None:
+            current_chunk += '\n' + l
+    return chunks
+
+
+def parse_files(files: List[IO]):
+    return [parse_file(f) for f in files]
+
+
+def parse_file(file: IO) -> dict:
+    text = extract_text(file)
+    case_number = extract_case_number(text)
+    text = clean_text(text)
+    chunks = extract_chunks(text)
+    dates = {}
+    for title, content in chunks.items():
+        dates[title] = [d.date().isoformat()
+                        for d in datefinder.find_dates(content)][0:DATES_COUNT[EVENT_TITLES.index(title)]]
+    return {"dates": dates, "case": case_number}
