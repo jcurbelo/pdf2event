f754e261b3f0b5a21d2609bf17d80b9f1244f6ca
jcurbelo <curbelorobin@gmail.com>
2021-08-16 14:42:28 -0400

    Add basic OAuth workflow

--- main.py
+++ main.py
@@ -3,10 +3,26 @@ from typing import List
 from fastapi import FastAPI, Request, UploadFile, File
 from fastapi.templating import Jinja2Templates
 from utils.pdf_parser import parse_files
+from starlette.middleware.sessions import SessionMiddleware
+from authlib.integrations.starlette_client import OAuth
+from starlette.config import Config
+from starlette.datastructures import Secret
+
+config = Config('.env')  # read config from .env file
+oauth = OAuth(config)
+oauth.register(
+    name='practice_panther',
+    api_base_url='https://app.practicepanther.com/',
+    authorize_url='https://app.practicepanther.com/Oauth/Authorize',
+    scope='full'
+)
 
 templates = Jinja2Templates(directory="templates")
 
+
 app = FastAPI()
+app.add_middleware(SessionMiddleware, secret_key=config(
+    'SECRET_KEY', cast=Secret))
 
 
 @app.post("/upload-files")
@@ -19,6 +35,19 @@ async def root(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
 
 
+@app.get("/auth/login")
+async def login_via_practice_panther(request: Request):
+    redirect_uri = request.url_for('auth')
+    return await oauth.practice_panther.authorize_redirect(request, redirect_uri)
+
+
+@app.route('/auth/callback')
+async def auth(request: Request):
+    token = await oauth.practice_panther.authorize_access_token(request)
+    return {"token": token}
+
 # returns file-like object
+
+
 def get_file_io(file: UploadFile):
     return BytesIO(file.file.read())
