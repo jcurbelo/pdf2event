3e50230970e5ab6a05a42e2b3e6b289730d38f47
jcurbelo <curbelorobin@gmail.com>
2021-08-19 18:34:13 -0400

    Finished complete MVP workflow

--- main.py
+++ main.py
@@ -10,26 +10,46 @@ from starlette.datastructures import Secret
 from starlette.responses import HTMLResponse, RedirectResponse
 from authlib.integrations.starlette_client import OAuth, OAuthError
 
-config = Config('.env')  # read config from .env file
+config = Config(".env")  # read config from .env file
 oauth = OAuth(config)
 oauth.register(
-    name='practice_panther',
-    api_base_url='https://app.practicepanther.com/',
-    authorize_url='https://app.practicepanther.com/oauth/authorize',
-    access_token_url='https://app.practicepanther.com/oauth/token',
-    scope='full'
+    name="practice_panther",
+    api_base_url="https://app.practicepanther.com/",
+    authorize_url="https://app.practicepanther.com/oauth/authorize",
+    access_token_url="https://app.practicepanther.com/oauth/token",
+    scope="full"
 )
+
 
 templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
 app.add_middleware(SessionMiddleware, secret_key=config(
-    'SECRET_KEY', cast=Secret))
+    "SECRET_KEY", cast=Secret))
 
 
 @app.post("/upload-files")
 async def upload_files(files: List[UploadFile] = File(...)):
     return {"result": parse_files([get_file_io(f) for f in files])}
+
+
+@app.post("/generate-events")
+async def generate_events(request: Request, cases: List[dict]):
+    oauth_values = request.session.get("oauth_values", {})
+    results = []
+    for case in cases:
+        for event in case["events"]:
+            for date in event["dates"]:
+                payload = {
+                    "subject": case["case"] + " - " + event["name"],
+                    "is_all_day": True,
+                    "start_date_time": date["value"],
+                    "end_date_time": date["value"]
+                }
+                resp = await oauth.practice_panther.post("/api/v2/events", data=payload, token=oauth_values)
+                results.append(resp.json())
+
+    return {"result": results}
 
 
 @app.get("/")
@@ -39,30 +59,31 @@ async def root(request: Request):
 
 @app.get("/auth/login")
 async def login_via_practice_panther(request: Request):
-    redirect_uri = request.url_for('auth')
+    redirect_uri = request.url_for("auth")
     return await oauth.practice_panther.authorize_redirect(request, redirect_uri)
 
 
-@app.route('/auth/callback')
+@app.route("/auth/callback")
 async def auth(request: Request):
     try:
         resp = await oauth.practice_panther.authorize_access_token(request)
     except OAuthError as error:
-        return HTMLResponse(f'<h1>{error.error}</h1>')
+        return HTMLResponse(f"<h1>{error.error}</h1>")
     oauth_values = {
-        'access_token': resp['access_token'],
-        'refresh_token': resp['refresh_token'],
-        'expires_in': resp['expires_in'],
-        'expires_at': resp['expires_at'],
+        "access_token": resp["access_token"],
+        "refresh_token": resp["refresh_token"],
+        "expires_in": resp["expires_in"],
+        "expires_at": resp["expires_at"],
+        "token_type": resp["token_type"],
     }
-    request.session['oauth_values'] = oauth_values
-    return RedirectResponse(url='/')
+    request.session["oauth_values"] = oauth_values
+    return RedirectResponse(url="/")
 
 
-@app.get('/auth/logout')
+@app.get("/auth/logout")
 async def logout(request: Request):
-    request.session.pop('oauth_values', None)
-    return RedirectResponse(url='/')
+    request.session.pop("oauth_values", None)
+    return RedirectResponse(url="/")
 
 # returns file-like object
 
